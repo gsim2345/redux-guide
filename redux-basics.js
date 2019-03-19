0fef67061373f146fe2ba4167c4f98586bc2fdf5
gsim2345 <gb@gabriellasimon.me>
2019-03-19 16:15:06 +0100

    Dispatched action

--- redux-basics.js
+++ redux-basics.js
@@ -12,6 +12,28 @@ const initialState = {
 // 2. The action 
 // ES6 feature, can define an inital value in the paramaters - if state is undefined, takes initialState instead. 
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        // DON'T DO LIKE THAT! IT'S MUTATING STATE:
+        //state.counter++
+        //return state;
+
+        // DO THAT INSTEAD: 
+        return {
+            // Fist copy old state: 
+            ...state,
+            // Then overwrite the property you want to adjust: 
+            counter: state.counter + 1
+            // returns updated state with updated counter
+        }
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state, 
+            counter: state.counter + action.value
+        }
+    }
+    
     return state;
 }
 
@@ -26,8 +48,14 @@ console.log(store.getState());
 
 
 // Dispatching action
-
-
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+//store.dispatch({type: 'ADD_COUNTER', payload: {}});
+// action is a js object, inluding property: type. 
+// type has to be all uppercase string type: 'INC_COUNTER' *increment counter*
+// conventionally it's short and descriptive
+// can be added any other properties. (value, id, name, whatever)
+console.log(store.getState());
 
 // Subscription
 
