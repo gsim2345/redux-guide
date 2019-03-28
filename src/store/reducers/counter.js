4d409e8a99cc1f51aaad2689990e1e2cff9cfbac
gsim2345 <gb@gabriellasimon.me>
2019-03-28 17:59:15 +0100

    refactor reducers - add utility (helper) function

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     counter: 0
@@ -8,31 +9,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.INCREMENT:
-            // returns immutably updated state 
-            // clone the old object
-            // can do with .assign
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1
-            return  newState;
+            return updateObject(state, {counter: state.counter + 1})
         case actionTypes.DECREMENT:
-            // or can do the cloning with the spread operator: 
-            return {
-                // another immutable solution
-                // not cloning first can lead to data loss, as what it returns will be the new state.
-                // doesn't merge with state like in the original React state without Redux
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, {counter: state.counter - 1})
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(state, {counter: state.counter + action.val})
         case actionTypes.SUBTRACT :
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
+            return updateObject(state, {counter: state.counter - action.val})
         // by default returns the current state
         default: return state
     }
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
-
+import { updateObject } from '../utility';
+ 
 const initialState = {
     results: []
 }
@@ -7,44 +8,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.STORE_RESULT: 
-            // Data changing logic can be also included here. 
-            // Better at put it here, into the reducer, than into the actions, so every logic is at one place
-            return {
-                ...state,
-                // concat is like push, but returns a new array, so it doesn't changes the original
-                // an immutable way of updating an array
-                // we can also add data transforming logic here: 
-                results: state.results.concat({id: new Date(), value: action.result * 2})
-                // if we are inside a reducer, and need to get to global state (like here we use value as state.counter), we need to get it as action payload => changed to action.result
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})})
         case actionTypes.DELETE_RESULT:
-            // normally we would do with splice, this however mutates the original array 
-            const id = 2;
-            //state.results.splice(id, 1);
-
-
-            // INSTEAD: 
-            // copy original array
-            const newArray = [...state.results];
-            // in this copy, if the elements are objects, they are still pointing to the same objects like they did before. So if we want to change something in an elemnt object, this copy is not enough. 
-            // For deleting an element it's enough though. 
-            newArray.splice(id, 1);
-
-            // OR - with FILTER
-            // we can use the filter method - returns a new array
-            // here returns those elements that don't have id as index 
-            //const updatedArray = state.results.filter((result, index) => index !== id);
-            // OR 
-            // resultElId => payload of action
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            console.log(updatedArray);
-            return {
-                ...state,
-                // updated copy of the original
-                //results: newArray
-                results: updatedArray
-            }
-        // by default returns the current state
+            return updateObject(state, {results: updatedArray});
         default: return state
     }
     
