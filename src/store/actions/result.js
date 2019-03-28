9b4adfaa9199b1a2096ed19c79fd3d6935d17205
gsim2345 <gb@gabriellasimon.me>
2019-03-28 17:25:20 +0100

    getState argument

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -21,9 +21,15 @@ export const saveResult = (res) => {
 
 export const storeResult = (res) => {
     // gets dispatch function  as argument
-    return dispatch => {
+    // if we decide to use actions for our logic, we can use getState function to get the current state:
+    // fx: want to save some data for a given user, and we need the id that is stored in the redux state. We can get the id first, and than reach out to the web.
+    // however it's better not to use it if we can avoid, or really just as a fallback, and not overuse it. Instead try to pass the data through the dispatches.  
+
+    return (dispatch, getState )=> {
         // we want to store the result after 2 seconds
         setTimeout(() => {
+            const oldCounter = getState().ctr.counter;
+            console.log(oldCounter);
             dispatch(saveResult(res));
         },2000);
     }
